38bafc6821ec73079c563c6b9a9ad4d1704f5532
Dru Serkes <druserkes@gmail.com>
2023-01-14 11:12:20 -0500

    Dynamically set the gradient

--- src/components/WeeklyRelativityBar/WeeklyRelativityBar.jsx
+++ src/components/WeeklyRelativityBar/WeeklyRelativityBar.jsx
@@ -1,11 +1,11 @@
 import './WeeklyRelativityBar.css'
 
 export const WeeklyRelativityBar = ({ lowerBound, upperBound, dayLow, dayHigh }) => {
-  const maxSaturation = 360;
-  const minSaturation = 180;
+  const minSaturation = 250;
   const left = `${dayLow - lowerBound}%`;
-  const width = `${dayHigh / upperBound * 100}%`
-
+  const width = `${(dayHigh / upperBound * 100) - (left.slice(0, -1))}%`
+  const gradientHigh = Math.round(Math.abs(dayHigh * (minSaturation / 100) - minSaturation));
+  const gradientLow = Math.round(Math.abs(dayLow * (minSaturation / 100) - minSaturation));
   function hslToHex(h, s, l) {
     l /= 100;
     const a = s * Math.min(l, 1 - l) / 100;
@@ -17,7 +17,7 @@ export const WeeklyRelativityBar = ({ lowerBound, upperBound, dayLow, dayHigh })
     return `#${f(0)}${f(8)}${f(4)}`;
   }
 
-  const background = `linear-gradient(90deg, ${hslToHex(minSaturation, 50, 50)}, ${hslToHex(maxSaturation, 50, 50)})`;
+  const background = `linear-gradient(90deg, ${hslToHex(gradientLow, 50, 50)}, ${hslToHex(gradientHigh, 50, 50)})`;
 
   return (
     <div className="WeeklyRelativityBar">
